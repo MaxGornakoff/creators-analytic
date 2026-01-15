999ea7d67953551c82bcf7df58edcdf7227b79cc
max.gornakoff <max.gornakoff@yandex.ru>
2026-01-15 13:26:25 +0300

    Add changes 14.01.2026

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,23 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
 // !!! ОБНОВИТЬ ЭТУ СТРОКУ !!!
-const NGROK_HOST = '9532b6c23918.ngrok-free.app'; 
+const NGROK_HOST = '9532b6c23918.ngrok-free.app';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: true, 
-    port: 5173, 
-    strictPort: true,
-    hmr: {
-        protocol: 'ws',
-        host: NGROK_HOST,
-    },
-    allowedHosts: [
-        NGROK_HOST
-    ]
-  }
+    base: '/creators-analytic/',
+    plugins: [react()],
+    server: {
+        host: true,
+        port: 5173,
+        strictPort: true,
+        hmr: {
+            protocol: 'ws',
+            host: NGROK_HOST,
+        },
+        allowedHosts: [
+            NGROK_HOST
+        ]
+    }
 })
